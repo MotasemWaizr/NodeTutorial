44286a0aa4202663ff1316e3f3cb4480bb1dfaa2
Motasem Alwazir <almotasem89@gmail.com>
2017-04-19 22:33:37 +0200

    add route sample

--- app.js
+++ app.js
@@ -3,8 +3,13 @@ var express=require('express');
 
 var app=express();
 
+
+app.get('/',function(req,res){
+    res.send("Hello world");
+})
+
 var port=5000;
 app.listen(port, function(err){
     console.log('running server on port '+ port);
-    
+
 });
