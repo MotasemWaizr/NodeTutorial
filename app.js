b89ab7a10319f5a301fb0e1d9474afb1052973f3
Motasem Alwazir <almotasem89@gmail.com>
2017-04-19 22:35:23 +0200

    Add second route

--- app.js
+++ app.js
@@ -6,7 +6,11 @@ var app=express();
 
 app.get('/',function(req,res){
     res.send("Hello world");
-})
+});
+
+app.get('/books',function(req,res){
+    res.send("Hello Books");
+});
 
 var port=5000;
 app.listen(port, function(err){
