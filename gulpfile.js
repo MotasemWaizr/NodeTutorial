fa647d5a831a6e9ff8893cb42cece2bfae71b0ee
MotasemWaizr <almotasem89@gmail.com>
2017-05-01 14:09:50 +0200

    Add inject task

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,7 @@ var jshint = require('gulp-jshint');//js hint to check syntax
 var jscs = require('gulp-jscs');
 var jsFile = ['*.js', 'src/**/*.js'];//JS file in the porject
 
+//Task to check the js file syntax and formatting
 gulp.task('style',function()  {
     return gulp.src(jsFile)
     .pipe(jshint())
@@ -10,4 +11,16 @@ gulp.task('style',function()  {
         verbose:true
     }))
     .pipe(jscs());
+});
+
+//inject css and js file to the html files
+gulp.task('inject',function() {
+    var wiredep = require('wiredep').stream;
+    var options = {
+        bowerJson:require('./bower.json'),//bower json
+        directory:'./public/lib'
+    };
+    return gulp.src('./src/views/*.html')
+    .pipe(wiredep(options))
+    .pipe(gulp.dest('./src/views'));
 });
