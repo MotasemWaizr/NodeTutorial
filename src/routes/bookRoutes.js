7e80a910ad88f14c78b251d00df7e4f710be7951
MotasemWaizr <almotasem89@gmail.com>
2017-05-01 22:13:51 +0200

    Change books

--- src/routes/bookRoutes.js
+++ src/routes/bookRoutes.js
@@ -2,52 +2,77 @@ var express = require('express');
 
 var bookRouter = express.Router();
 var books = [
-{
-    title : 'The Lightning Thief',
-    author : 'Rick Riordan',
-    genre: 'Historical Fiction',
-    read: false
-},
-{
-    title : 'The Sea of Monsters',
-    author : 'Rick Riordan',
-    genre: 'Historical Fiction',
-    read: false
-},
-{
-    title : 'Sophie\'s World : The Greek Philosophers',
-    author : 'Jostein Gaarder',
-    genre: 'Historical Fiction',
-    read: false
-},
-{
-    title : 'Lucene in Action, Second Edition',
-    author : 'Michael McCandless',
-    genre: 'Historical Fiction',
-    read: false
-}
+    {
+        title: 'War and Peace',
+        genre: 'Historical Fiction',
+        author: 'Lev Nikolayevich Tolstoy',
+        read: false
+    },
+    {
+        title: 'Les Misérables',
+        genre: 'Historical Fiction',
+        author: 'Victor Hugo',
+        read: false
+    },
+    {
+        title: 'The Time Machine',
+        genre: 'Science Fiction',
+        author: 'H. G. Wells',
+        read: false
+    },
+    {
+        title: 'A Journey into the Center of the Earth',
+        genre: 'Science Fiction',
+        author: 'Jules Verne',
+        read: false
+    },
+    {
+        title: 'The Dark World',
+        genre: 'Fantasy',
+        author: 'Henry Kuttner',
+        read: false
+    },
+    {
+        title: 'The Wind in the Willows',
+        genre: 'Fantasy',
+        author: 'Kenneth Grahame',
+        read: false
+    },
+    {
+        title: 'Life On The Mississippi',
+        genre: 'History',
+        author: 'Mark Twain',
+        read: false
+    },
+    {
+        title: 'Childhood',
+        genre: 'Biography',
+        author: 'Lev Nikolayevich Tolstoy',
+        read: false
+    }
 ];
 
 //To get here the route must be /Books/
 bookRouter.route('/')
-    .get(function(req,res) {
-    res.render('books',{title:'Hello from ejs',
-        nav:[
-            {
-                Link:'/Books',
-                Text:'Books'
-            }, {
-                Link:'/Authors',
-                Text:'Authors'
-            },
+    .get(function (req, res) {
+        res.render('books', {
+            title: 'Hello from ejs',
+            nav: [
+                {
+                    Link: '/Books',
+                    Text: 'Books'
+                }, {
+                    Link: '/Authors',
+                    Text: 'Authors'
+                },
             ],
-        books:books
+            books: books
+        });
     });
-});
 
 //To get her the route must be /Books/single
 bookRouter.route('/single')
-    .get(function(req,res) {
-    res.send('Hello Single  Book');
-});
+    .get(function (req, res) {
+        res.send('Hello Single  Book');
+    });
 module.exports = bookRouter;
