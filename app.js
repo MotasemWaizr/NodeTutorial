5659ede9fe80b5c1165d4ef6dec16912024572c8
MotasemWaizr <almotasem89@gmail.com>
2017-05-01 17:08:51 +0200

    Pass parameters from app.js to html

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ app.set('views', './src/views');
 app.set('view engine', 'jade');
 
 app.get('/',function(req,res) {
-    res.render('jade/index');
+    res.render('jade/index',{list:[1,2,3,4,5]});
 });
 
 app.get('/books',function(req,res) {
